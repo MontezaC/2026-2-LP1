8fb1eec0155dcd6b7ba5b3abedc4f1aef747725e
MontezaC <c.monteza.a@uni.pe>
2026-09-09 18:46:08 -0500

    Add files via upload

---
+++ SEM01/holamundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	printf("Vuelo con este curso\n");
+	printf("Me encanta la UNI\n");
+	return 0;
+}
---
+++ SEM02/hola.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int edad=25;
+    float altura=1.75;
+    char inicial='j';
+
+    printf("Hola mundo en C\n");
+    printf("Edad: %d anios \n",edad);
+    printf("Altura: %.2f metros\n",altura);
+    printf("Inicial: %c\n",inicial);
+
+    return 0;
+}
